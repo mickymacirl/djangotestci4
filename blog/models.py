78226c190e466816482f70622f48009cdc04d3ef
mickymacirl <mickymacster@gmail.com>
2022-12-11 22:33:32 +0000

    add base

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,19 @@ STATUS = (
     (1,"Publish")
 )
 
+# The Post model has a title, slug, author, updated_on, content, created_on, and status.
+# The title and slug are defined as character fields of 200 characters each. The title is also set as
+# unique.
+# The author is a foreign key. This means that each post is written by a user and is related to a
+# user.
+# The updated_on and created_on fields are set to the current date and time.
+# The status field is an integer field with choices defined in the STATUS variable.
+# The content field is a text field.
+# The slug field is also set as unique.
+# The updated_on field is set to the current date and time.
+# The status field is an integer field with choices defined in the STATUS variable.
+# The content field is a text field.
+# The slug field is also set as unique
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
