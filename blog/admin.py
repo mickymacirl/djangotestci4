c4ea35a7be151b387587be1a38f846e1ce3529ec
mickymacirl <mickymacster@gmail.com>
2022-12-17 11:56:19 +0000

    add model for post

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,57 @@ from .models import Post
 # The PostAdmin class inherits from ModelAdmin, and defines a list_display, list_filter,
 # search_fields, and prepopulated_fields
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status','created_on')
-    list_filter = ("status",)
-    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'status', 'author', 'created_on', 'category',)
+    list_filter = ("status", "author", "created_on", 'category',)
+    search_fields = ['title', 'content', 'category',]
     prepopulated_fields = {'slug': ('title',)}
+
+    # Add an action to mark posts as published
+    def make_published(self, request, queryset):
+        rows_updated = queryset.update(status='1')
+        if rows_updated == 1:
+            message_bit = "1 post was"
+        else:
+            message_bit = f"{rows_updated} posts were"
+        self.message_user(request, f"{message_bit} successfully published!")
+    make_published.short_description = "Set selected posts to published"
+
+    # Add an action to mark posts as drafts
+    def make_draft(self, request, queryset):
+        rows_updated = queryset.update(status='0')
+        if rows_updated == 1:
+            message_bit = "1 post was"
+        else:
+            message_bit = f"{rows_updated} posts were"
+        self.message_user(request, f"{message_bit} successfully marked as drafts.")
+    make_draft.short_description = "Set selected posts to draft"
+
+    # Add a save and continue editing button on the edit form
+    save_on_top = True
+    save_as = True
+    actions = ['make_published', 'make_draft']
+
+    # Add an action to mark posts as disabled
+    def make_disabled(self, request, queryset):
+        rows_updated = queryset.update(status='3')
+        if rows_updated == 1:
+            message_bit = "1 post was"
+        else:
+            message_bit = f"{rows_updated} posts were"
+        self.message_user(request, f"{message_bit} successfully marked as disabled.")
+        make_disabled.short_description = "Set selected posts as disabled"
+
+    # Add a save and continue editing button on the edit form
+    save_on_top = True
+    save_as = True
+    actions = ['make_published', 'make_draft', 'make_disabled']
+#class CategoryAdmin(admin.ModelAdmin):
+#    list_display = ('title', 'slug', 'description', 'status_cat', 'updated_on')
+#    list_filter = ("title", "slug", "description", "status_cat", "updated_on")
+#    search_fields = ['title', 'description']
+#    prepopulated_fields = {'slug': ('title',)}
+
 
 # Registering the Post model with the PostAdmin class.
 admin.site.register(Post, PostAdmin)
+# admin.site.register(Category, CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,32 +2,42 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# A tuple of tuples.
 STATUS = (
     (0,"Draft"),
-    (1,"Publish")
+    (1,"Publish"),
+    (2,"Disabled"),
 )
 
-# The Post model has a title, slug, author, updated_on, content, created_on, and status.
-# The title and slug are defined as character fields of 200 characters each. The title is also set as
-# unique.
-# The author is a foreign key. This means that each post is written by a user and is related to a
-# user.
-# The updated_on and created_on fields are set to the current date and time.
-# The status field is an integer field with choices defined in the STATUS variable.
-# The content field is a text field.
-# The slug field is also set as unique.
-# The updated_on field is set to the current date and time.
-# The status field is an integer field with choices defined in the STATUS variable.
-# The content field is a text field.
-# The slug field is also set as unique
+# STATUS_CAT = (
+#    (0,"OFF"),
+#    (1,"ON"),
+#    (2,"Disabled"),
+#)
+
+#class Category(models.Model):
+#    title = models.CharField(max_length=50)
+#    description = models.TextField(blank=True)
+#    slug = models.SlugField(max_length=50, unique=True)
+#    updated_on = models.DateTimeField(auto_now= True)
+#    status_cat = models.IntegerField(choices=STATUS_CAT, default=2)
+
+#    class Meta:
+#        ordering = ['-updated_on']
+
+#    def __str__(self):
+#        return self.title
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
+    category = models.CharField(max_length=200, unique=False)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    status = models.IntegerField(choices=STATUS, default=2)
 
     class Meta:
         ordering = ['-created_on']
